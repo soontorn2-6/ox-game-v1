a218e61acbfb7389d5884fb01d0e93a1964d61dc
soontorn2-6 <soontorn.good@gmail.com>
2021-05-19 11:12:31 +0700

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="m-3">
+    Test
+    <div>No Ref : {{ number + 1 }}</div>
+    <div>Ref : {{ refNumber }}</div>
+    <div>Reactive : {{ reactiveNumber }}</div>
+  </div>
+</template>
+<script>
+import { ref, reactive,toRefs } from "vue";
+export default {
+  setup() {
+    let number = 1;
+    const refNumber = ref(1);
+    const reactiveNumber = reactive({ count: 1 });
+    const {count} = toRefs(reactiveNumber)
+    setInterval(() => {
+      number += 1;
+      refNumber.value += 1;
+      reactiveNumber.count += 1;
+    }, 1000);
+    return { number, refNumber, reactiveNumber };
+  },
+};
+</script>
